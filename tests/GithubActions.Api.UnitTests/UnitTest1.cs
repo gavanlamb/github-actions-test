2a977ac5c9676f902d7184ca8db9590336bcb311
Gavan Lamb <gavan.jon.lamb@gmail.com>
2023-06-13 20:42:59 +1000

    Add unit tests

--- tests/GithubActions.Api.UnitTests/UnitTest1.cs
+++ tests/GithubActions.Api.UnitTests/UnitTest1.cs
@@ -5,7 +5,58 @@ namespace GithubActions.Api.UnitTests;
 public class UnitTest1
 {
     [Fact]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
     public void Test1()
     {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    public void Test2()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    public void Test3()
+    {
+        Assert.True(true);
+    }
+    
+    [Theory]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    [InlineData()]
+    public void Test4()
+    {
+        Assert.True(true);
     }
 }
