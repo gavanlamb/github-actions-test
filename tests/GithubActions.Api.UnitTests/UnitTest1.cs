f2e1197d26746f804de8974a1ded26503dfea795
Gavan Lamb <gavan.jon.lamb@gmail.com>
2023-06-13 22:22:41 +1000

    Add coverage tests

--- tests/GithubActions.Api.UnitTests/UnitTest1.cs
+++ tests/GithubActions.Api.UnitTests/UnitTest1.cs
@@ -1,62 +1,42 @@
+using System;
 using Xunit;
 
 namespace GithubActions.Api.UnitTests;
 
 public class UnitTest1
 {
-    [Theory]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
+    [Fact]
     public void Test1()
     {
         Assert.True(true);
     }
     
-    [Theory]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
+    [Fact]
     public void Test2()
     {
         Assert.True(true);
     }
     
-    [Theory]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
+    [Fact]
     public void Test3()
     {
         Assert.True(true);
     }
     
-    [Theory]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
-    [InlineData()]
+    [Fact]
     public void Test4()
     {
+        var weatherForecast = new WeatherForecast
+        {
+            Date = DateTime.Today,
+            Summary = "wooo",
+            TemperatureC = 100
+        };
+        
         Assert.True(true);
+        Assert.Equal(weatherForecast.Date, weatherForecast.Date);
+        Assert.Equal(weatherForecast.Summary, weatherForecast.Summary);
+        Assert.Equal(weatherForecast.TemperatureC, weatherForecast.TemperatureC);
+        Assert.Equal(weatherForecast.TemperatureF, weatherForecast.TemperatureF);
     }
 }
